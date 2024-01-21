2079d8e8387d83093f0b043973a40adfa653844e
sumiyuru priyasara <sumiyurupriyasara@gmail.com>
2024-01-21 15:13:23 +0530

    add new menu

--- content/_index.md
+++ content/_index.md
@@ -3,15 +3,15 @@ title: Home page
 layout: home
 
 hero_section:
-  name: Who’s Roman?
+  name: Hello! <br> I’m Roman.
   bio: "Shaping the way people use and experience technology to interact
     with the world today and in the future with a huge passion for
     intuitive user experiences"
   titles:
-    - Head of Product Design and UX,
-    - 18+ years of User Interaction Experience (UX/UI),
-    - 15+ Years of Leading Web-Based Software Engineering and Agile/Scrum Development,
-    - 4 Years of Managing a Team of Remote UI Designers and Mentoring a Team of Product Managers,
+    - UI/UX,
+    - Software Engineering,
+    - Agile/Scrum Development,
+    - Managing a Team,
 
   image: /assets/img/profile-img.jpg
 ---
--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -35,29 +35,6 @@
   };
 
   /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select("#navbar .scrollto", true);
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200;
-    navbarlinks.forEach((navbarlink) => {
-      if (!navbarlink.hash) return;
-      let section = select(navbarlink.hash);
-      if (!section) return;
-      if (
-        position >= section.offsetTop &&
-        position <= section.offsetTop + section.offsetHeight
-      ) {
-        navbarlink.classList.add("active");
-      } else {
-        navbarlink.classList.remove("active");
-      }
-    });
-  };
-  window.addEventListener("load", navbarlinksActive);
-  onscroll(document, navbarlinksActive);
-
-  /**
    * Scrolls to an element with header offset
    */
   const scrollto = (el) => {
@@ -83,15 +60,6 @@
     window.addEventListener("load", toggleBacktotop);
     onscroll(document, toggleBacktotop);
   }
-
-  /**
-   * Mobile nav toggle
-   */
-  on("click", ".mobile-nav-toggle", function (e) {
-    select("body").classList.toggle("mobile-nav-active");
-    this.classList.toggle("bi-list");
-    this.classList.toggle("bi-x");
-  });
 
   /**
    * Scrool with ofset on links with a class name .scrollto
@@ -171,91 +139,6 @@
   }
 
   /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener("load", () => {
-    let portfolioContainer = select(".portfolio-container");
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: ".portfolio-item",
-      });
-
-      let portfolioFilters = select("#portfolio-flters li", true);
-
-      on(
-        "click",
-        "#portfolio-flters li",
-        function (e) {
-          e.preventDefault();
-          portfolioFilters.forEach(function (el) {
-            el.classList.remove("filter-active");
-          });
-          this.classList.add("filter-active");
-
-          portfolioIsotope.arrange({
-            filter: this.getAttribute("data-filter"),
-          });
-          portfolioIsotope.on("arrangeComplete", function () {
-            AOS.refresh();
-          });
-        },
-        true
-      );
-    }
-  });
-
-  /**
-   * Initiate portfolio lightbox
-   */
-  const portfolioLightbox = GLightbox({
-    selector: ".portfolio-lightbox",
-  });
-
-  /**
-   * Initiate portfolio details lightbox
-   */
-  const portfolioDetailsLightbox = GLightbox({
-    selector: ".portfolio-details-lightbox",
-    width: "90%",
-    height: "90vh",
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper(".portfolio-details-slider", {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
-
-  /**
    * Animation on scroll
    */
   window.addEventListener("load", () => {
@@ -266,6 +149,14 @@
       mirror: false,
     });
   });
+
+  /* Sidebar toggle
+   */
+  if (select(".toggle-sidebar-btn")) {
+    on("click", ".toggle-sidebar-btn", function (e) {
+      select("body").classList.toggle("toggle-sidebar");
+    });
+  }
 
   /**
    * Initiate Pure Counter
